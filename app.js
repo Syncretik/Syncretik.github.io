74b5853505b780f8ece75b6673ed1a8451ffdb85
Patrick Wu <scorpio7500@gmail.com>
2015-11-25 07:46:12 +0000

    added About section

---
+++ app.js
@@ -0,0 +1,19 @@
+function loadHome() {
+
+}
+
+function loadAbout() {
+
+}
+
+function loadWorks() {
+
+}
+
+var home 	= document.getElementById('home'),
+	about 	= document.getElementById('about'),
+	works 	= document.getElementById('works');
+
+home.addEventListener('click', loadHome());
+about.addEventListener('click', loadAbout());
+works.addEventListener('click', loadWorks());
