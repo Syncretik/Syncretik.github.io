5f63f61578a822b8166f7e84dcf5ebbf2c212f36
Patrick Wu <scorpio7500@gmail.com>
2015-11-27 06:30:30 +0000

    added twitter icon to footer, hover transition

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
+window.onload = function() {
 
+	function toggleSection(section) {
+		console.log(section);
+		var display = section.style.display;
+		console.log(display);
+		var otherDisplays = allSections.style.display;
+		if (display == 'none') {
+			display = 'flex';
+			for (var i in otherDisplays) {
+				if (otherDisplays[i] == section) {
+					continue;
+				}
+				otherDisplays[i] = 'none';
+			}
+		}
+	}
 
-function loadHome() {
+	var home = document.getElementById('home'),
+		about = document.getElementById('about'),
+		works = document.getElementById('works');
 
+	var home = document.getElementById('home-panel'),
+		about = document.getElementById('about-panel'),
+		works = document.getElementById('works-panel'),
+		allSections = document.getElementsByClassName('info-panel');
+
+	home.addEventListener('click', toggleSection(home));
+	about.addEventListener('click', toggleSection(about));
+	works.addEventListener('click', toggleSection(works));
 }
-
-function loadAbout() {
-
-}
-
-function loadWorks() {
-
-}
-
-var home 	= document.getElementById('home'),
-	about 	= document.getElementById('about'),
-	works 	= document.getElementById('works');
-
-
-
-home.addEventListener('click', loadHome());
-about.addEventListener('click', loadAbout());
-works.addEventListener('click', loadWorks());
